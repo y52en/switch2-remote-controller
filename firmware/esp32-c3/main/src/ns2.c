9a3cb27a675ab8ebefcc9d940c671d7632fcd37d
y52en <61645319+y52en@users.noreply.github.com>
2026-08-20 13:42:49 +0900

    fix: include NimBLE host definitions

--- firmware/esp32-c3/main/src/ns2.c
+++ firmware/esp32-c3/main/src/ns2.c
@@ -1,5 +1,7 @@
 #include "console.h"
 
+#include "host/ble_hs.h"
+
 console_ns2_t g_console_ns2s[CONTROLLER_SLOT_COUNT] = {
     [0 ... CONTROLLER_SLOT_COUNT - 1] = {
         .ble_addr = { .type = BLE_ADDR_PUBLIC, .val = { 0 } },
